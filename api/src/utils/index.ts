b94a31c530bea8ba34841ab7a334b7a1a9bf07d7
LayNath242 <nath.selendra@gmail.com>
2022-09-01 16:52:25 +0700

    add: node provider

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -1,2 +1,15 @@
 export { default as config } from './config';
 export { default as logger } from './logger';
+
+/* eslint no-promise-executor-return: "off" */
+export const wait = async (ms: number): Promise<void> => new Promise((res) => setTimeout(res, ms));
+
+export const max = (...args: number[]): number => {
+  if (args.length === 0) {
+    throw new Error('Given array is empty!');
+  }
+  return args.reduce(
+    (prev, current) => (prev > current ? prev : current),
+    args[0],
+  );
+};
